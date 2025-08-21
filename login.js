4715c6b6bfa5f53bc15ef681f23a3078cd1bdf00
ashvin <ashvinomprakashz24@gmail.com>
2025-08-21 12:09:19 +0530

    Updated login.js with new message

--- login.js
+++ login.js
@@ -1,1 +1,42 @@
-console.log('hi hello');
+// console.log('hi hello');
+// require('dotenv').config();
+// const express=require ('express');
+// const app=express();
+// const path = require('path');
+// const connectDB = require('./src/config/db');
+// const userRoutes=require('./src/routes/user');
+
+// connectDB();
+
+// const PORT=process.env.PORT || 3000;
+// // app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
+// app.use('/uploads', express.static(path.join('public/uploads')));
+
+// app.use(express.json());
+
+// app.use('/users', userRoutes);
+// app.use('/auth', require('./src/routes/auth'));
+
+console.log("Login feature is under development!");
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
